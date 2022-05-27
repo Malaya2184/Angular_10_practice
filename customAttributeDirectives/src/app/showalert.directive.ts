91d7772edb01a934b79a8c54c8b778565ef5f58b
Malaya2184 <malayakumarswain8@gmail.com>
2022-05-27 16:42:35 +0530

    host listener made easy

--- customAttributeDirectives/src/app/showalert.directive.ts
+++ customAttributeDirectives/src/app/showalert.directive.ts
@@ -7,20 +7,35 @@ export class ShowalertDirective {
 
   constructor(private er : ElementRef, private renderer: Renderer2) {
 
-    er.nativeElement.style.color = 'blue'
+    this.er.nativeElement.style.color = 'blue'
     er.nativeElement.style.backgroundColor= "yellow"
-    renderer.listen(er.nativeElement,'mouseover', function(){
-      er.nativeElement.style.color = 'red'
-      er.nativeElement.style.cursor = 'pointer'
-      er.nativeElement.style.fontSize = '30px'
-    })
-    renderer.listen(er.nativeElement,'mouseout', function(){
-      er.nativeElement.style.color = 'green'
-      er.nativeElement.style.fontSize = '20px'
-    })
-    renderer.listen(er.nativeElement,'click', function(){
-    alert(er.nativeElement.innerHTML)
-    })
+    // renderer.listen(er.nativeElement,'mouseover', function(){
+    //   er.nativeElement.style.color = 'red'
+    //   er.nativeElement.style.cursor = 'pointer'
+    //   er.nativeElement.style.fontSize = '30px'
+    // })
+    // renderer.listen(er.nativeElement,'mouseout', function(){
+    //   er.nativeElement.style.color = 'green'
+    //   er.nativeElement.style.fontSize = '20px'
+    // })
+    // renderer.listen(er.nativeElement,'click', function(){
+    // alert(er.nativeElement.innerHTML)
+    // })
    }
 
+   @HostListener('click')
+   onMouseClick(){
+     alert('host listener mouse clicked')
+   }
+   @HostListener('mouseover')
+   onMouseover(){
+     this.er.nativeElement.style.color = 'red'
+     this.er.nativeElement.style.fontSize = '30px'
+     this.er.nativeElement.style.cursor = 'pointer'
+   }
+   @HostListener('mouseout')
+   onMouseout(){
+    this.er.nativeElement.style.color = 'green'
+    this.er.nativeElement.style.fontSize = '20px'
+   }
 } 
