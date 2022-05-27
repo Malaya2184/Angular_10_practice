ab0244a6e46b313a4148a2fce33e912e06c2bcea
Malaya2184 <malayakumarswain8@gmail.com>
2022-05-27 14:40:11 +0530

    renderer and element ref used for listener and styles

--- customAttributeDirectives/src/app/showalert.directive.ts
+++ customAttributeDirectives/src/app/showalert.directive.ts
@@ -5,6 +5,22 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 })
 export class ShowalertDirective {
 
-  constructor() { }
+  constructor(private er : ElementRef, private renderer: Renderer2) {
 
-}
+    er.nativeElement.style.color = 'blue'
+    er.nativeElement.style.backgroundColor= "yellow"
+    renderer.listen(er.nativeElement,'mouseover', function(){
+      er.nativeElement.style.color = 'red'
+      er.nativeElement.style.cursor = 'pointer'
+      er.nativeElement.style.fontSize = '30px'
+    })
+    renderer.listen(er.nativeElement,'mouseout', function(){
+      er.nativeElement.style.color = 'green'
+      er.nativeElement.style.fontSize = '20px'
+    })
+    renderer.listen(er.nativeElement,'click', function(){
+    alert("Hello World")
+    })
+   }
+
+} 
