0f39722b06240ba7ebc97d96071ae2c36f88667d
Malaya2184 <malayakumarswain8@gmail.com>
2022-06-02 00:28:50 +0530

    angular simple http req and res

---
+++ HttpModule/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, DoCheck, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'HttpModule';
+
+  empUrl = 'http://dummy.restapiexample.com/api/v1/employees'
+  employees: any;
+  constructor(private http : HttpClient){
+
+
+  }
+ ngOnInit(){
+  this.getEmp().subscribe( (emp: any) => {
+    this.employees = emp
+  // console.log(this.employees);
+  })
+  
+  // console.log(JSON.stringify(this.employees));
+
+}
+
+
+  getEmp():any{
+
+    return this.http.get(this.empUrl)
+  }
+ 
+  
+
+}
